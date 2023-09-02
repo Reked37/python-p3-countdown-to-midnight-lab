f500f67198c347edfedd0d85c6ff41f511c791cb
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-02 11:51:36 -0500

    lab completed

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import time
+def countdown(timer):
+    while timer<10:
+        print(f'{timer} SECOND(S)!')
+        timer -=1
+        if timer == 0:
+            return print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(timer):
+    while timer <10:
+        print(f'{timer} SECOND(S)!')
+        time.sleep(1)
+        timer -=1
+        if timer == 0:
+            return print('HAPPY NEW YEAR!')
